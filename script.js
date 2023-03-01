021c5cfc95d3e355f3ffae2e03e48d6241f20206
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-03-01 21:59:02 +0530

    ans

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+//complete this code
+class Animal {}
+
+class Dog extends Animal {}
+
+class Cat extends Animal {}
+
+// Do not change the code below this line
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
